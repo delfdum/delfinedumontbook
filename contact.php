b192d937837ae8dab1fa85bdca92641cd2df1c38
delfdum <60744187+delfdum@users.noreply.github.com>
2020-06-21 20:18:36 +0200

    changement page contact

--- contact.php
+++ contact.php
@@ -3,14 +3,14 @@ include "config.php";
 include PATH_TEMPLATE . "include/head.php"; ?>
 
 <main>
-<h1> Me contacter</h1>
-
-<h2> par mail: <?php echo MontrerContenu('MAIL')?></h2>
-<h2> par téléphone: <?php echo MontrerContenu('TELEPHONE')?></h2>
-<h2><div class="">
-<a href="<?php echo MontrerContenu('LINKEDIN')?>" target="_blank"><img src="https://img.icons8.com/fluent/48/000000/linkedin.png"/></a>
-
-</div></h2>
+  <h1> Me contacter</h1>
+    <div class="contenair_contact">
+      <h2> par mail: <?php echo MontrerContenu('MAIL')?></h2> <br>
+      <h2> par téléphone: <?php echo MontrerContenu('TELEPHONE')?></h2><br>
+      <h2>
+        <a href="<?php echo MontrerContenu('LINKEDIN')?>" target="_blank"><img class="logoContact" src="https://img.icons8.com/fluent/48/000000/linkedin.png"/></a>
+      </h2>
+  </div>
 </main>
 
 <?php include PATH_TEMPLATE . "include/footer.php"; ?>
