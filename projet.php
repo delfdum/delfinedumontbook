05e6402019325fd6efef0ee838e40e4770ddb2f6
delfdum <60744187+delfdum@users.noreply.github.com>
2020-06-21 16:04:31 +0200

    creation page projet

---
+++ projet.php
@@ -0,0 +1,46 @@
+<?php include "config.php";
+
+
+function unProjet ($idProjet) {
+    // retourne toutes les informations du menu qui a comme identifiant $idProjet
+
+    global $bdd;
+
+    $query = $bdd -> prepare("select * from projet where id_projet = :idProjet");
+
+    $query -> execute([":idProjet" => $idProjet]);//
+
+    return $query -> fetch(PDO::FETCH_ASSOC); // on utilise fetch et non fetchAll car nous souhaitons retourner un seul résultat.
+}
+ $projet_a_afficher = unProjet($_GET["projetChoisi"]);
+
+//  echo "<pre>";
+// var_dump($projet_a_afficher);
+//  echo "<pre>";
+//exit;
+
+
+
+ function lesTechnos ($idTechno) {
+
+     global $bdd;
+
+     $query2 = $bdd -> prepare("SELECT nom_techno
+       FROM projet, technologie, projet_techno
+       WHERE projet.id_projet = projet_techno.projet_id
+       AND projet_techno.techno_id = technologie.id_techno
+       AND projet.id_projet = :idProjet ");
+
+     $query2 -> execute([":idProjet" => $idTechno]);//
+
+     return $query2 -> fetchAll(PDO::FETCH_ASSOC); // on utilise fetch et non fetchAll car nous souhaitons retourner un seul résultat.
+ }
+ $techno_a_afficher = lesTechnos($_GET["projetChoisi"]);
+
+// var_dump($techno_a_afficher[0]["nom_techno"]);
+// echo $techno_a_afficher[1]["nom_techno"];
+ // echo "<pre>";
+ // var_dump($techno_a_afficher);
+ // echo "<pre>";
+
+ include PATH_TEMPLATE . "projet_par_page.php";
